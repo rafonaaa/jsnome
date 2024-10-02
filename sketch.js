346f07bbca305848b6aa35b834c4d332f7a7a640
rafonaaa <alicia.biscola@escola.pr.gov.br>
2024-10-02 13:52:52 -0300

    adicionar arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 6000);
+  }
+  
+  function draw() {
+    background(220);
+  
+    stroke("blue")
+   fill("red")
+  
+  console.log(mouseIsPressed);
+  rect(mouseX,  mouseY, 20, 35);
+  
+  
